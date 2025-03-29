9c975cce60a00b1e8e61358c3f393a5476f0250b
Micah1001 <167143423+Micah1001@users.noreply.github.com>
2025-03-29 01:02:07 +0000

    V7

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -13,28 +13,22 @@ preloader.classList.add('fade-out');
 let slideIndex = [1, 1, 1, 1, 
                   1, 1, 1, 1, 
                   1, 1, 1, 1, 
-                  1, 1, 1, 1];
+                  1, 1, 1, 1,
+                  1, 1, 1, 1,
+                  1, 1, 1, 1,];
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", 
                "mySlides5", "mySlides6", "mySlides7", "mySlides8", 
                "mySlides9", "mySlides10", "mySlides11","mySlides12", 
-               "mySlides13", "mySlides14", "mySlides15", "mySlides16"];
+               "mySlides13", "mySlides14", "mySlides15", "mySlides16",
+               "mySlides17", "mySlides18", "mySlides19", "mySlides20",
+               "mySlides21", "mySlides22", "mySlides23", "mySlides24",];
 
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-showSlides(1, 4);
-showSlides(1, 5);
-showSlides(1, 6);
-showSlides(1, 7);
-showSlides(1, 8);
-showSlides(1, 9);
-showSlides(1, 10);
-showSlides(1, 11);
-showSlides(1, 12);
-showSlides(1, 13);
-showSlides(1, 14);
-showSlides(1, 15);
+showSlides(1, 0); showSlides(1, 1); showSlides(1, 2); showSlides(1, 3);
+showSlides(1, 4); showSlides(1, 5); showSlides(1, 6); showSlides(1, 7);
+showSlides(1, 8); showSlides(1, 9); showSlides(1, 10); showSlides(1, 11);
+showSlides(1, 12); showSlides(1, 13); showSlides(1, 14);showSlides(1, 15);
+showSlides(1, 16); showSlides(1, 17); showSlides(1, 18); showSlides(1, 19);
+showSlides(1, 20); showSlides(1, 21); showSlides(1, 22); showSlides(1, 23);
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
